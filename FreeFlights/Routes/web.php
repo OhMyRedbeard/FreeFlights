b07915264309e72ebba4ef2f2a5cc9ea895bc1f9
OhMyRedbeard <ohmyredbeardttv@gmail.com>
2025-09-19 14:05:24 -0400

    Update web.php

--- FreeFlights/Routes/web.php
+++ FreeFlights/Routes/web.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::group(['middleware' => ['auth']], function () {
-    Route::prefix('freeflights')->group(function() {
+    Route::prefix('newflight')->group(function() {
         Route::get('/', 'FreeFlightsController@index')->name('freeflights.index');
         Route::post('/create', 'FreeFlightsController@create')->name('freeflights.create');
         Route::get('/generate-flight-number', 'FreeFlightsController@generateFlightNumber')->name('freeflights.generate-flight-number');
